d966ff1472fd8bb90712369c7e8aabee7ab9224d
John Ottenlips <johnottenlips@Johns-Mac-mini.attlocal.net>
2023-05-06 23:19:01 -0500

    initial commit ✌️

---
+++ index.js
@@ -0,0 +1,40 @@
+const people = { john: [], adrienne: [] };
+const personList = Object.keys(people);
+const chores = [
+  "cat",
+  "meals",
+  "groceries",
+  "pickup",
+  "bathrooms",
+  "mow",
+  "garden",
+  "dishes",
+  "vacuum",
+  "surfaces",
+];
+const getRandomItem = (items) =>
+  items[Math.floor(Math.random() * items.length)];
+
+const assignChores = (chores) => {
+  const choreWheel = chores.reduce(
+    (acc, cur, index) => {
+      const chore = getRandomItem(acc.unassignedChores);
+      const choresLeft = acc.unassignedChores.filter((item) => item !== chore);
+      const personList = Object.keys(people);
+      const person = personList[index % 2];
+      return {
+        ...acc,
+        [person]: [...acc[person], chore],
+        unassignedChores: choresLeft,
+      };
+    },
+    {
+      ...people,
+      unassignedChores: chores,
+    }
+  );
+  return personList
+    .map((person) => `${person}: ${choreWheel[person].join(", ")}`)
+    .join(`\n`);
+};
+console.log(assignChores(chores));
